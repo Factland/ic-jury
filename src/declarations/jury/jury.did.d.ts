36b0d19deb7e5f3eea4b566dbcdc72b71065e3ba
John Plevyak <jplevyak@gmail.com>
2023-08-20 19:41:04 +0800

    Update declarations.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- src/declarations/jury/jury.did.d.ts
+++ src/declarations/jury/jury.did.d.ts
@@ -2,38 +2,50 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
 export interface Block {
-  'certificate' : Uint8Array,
+  'certificate' : Uint8Array | number[],
   'data' : Data,
-  'tree' : Uint8Array,
-  'previous_hash' : Uint8Array,
+  'tree' : Uint8Array | number[],
+  'previous_hash' : Uint8Array | number[],
 }
 export interface Data {
-  'jurors' : Array<Uint8Array>,
+  'jurors' : Array<Uint8Array | number[]>,
   'kind' : Kind,
-  'memo' : Uint8Array,
-  'rand' : [] | [Uint8Array],
+  'memo' : Uint8Array | number[],
+  'rand' : [] | [Uint8Array | number[]],
 }
 export type Kind = { 'Add' : null } |
   { 'Remove' : null } |
   { 'Select' : null } |
   { 'Expand' : null };
 export interface _SERVICE {
-  'add' : ActorMethod<[Array<Uint8Array>, Uint8Array], number>,
+  'add' : ActorMethod<
+    [Array<Uint8Array | number[]>, Uint8Array | number[]],
+    number
+  >,
   'authorize' : ActorMethod<[Principal], undefined>,
-  'commit' : ActorMethod<[Uint8Array], [] | [number]>,
+  'commit' : ActorMethod<[Uint8Array | number[]], [] | [number]>,
   'deauthorize' : ActorMethod<[Principal], undefined>,
-  'expand' : ActorMethod<[number, number, Uint8Array], number>,
-  'find' : ActorMethod<[number, Array<Uint8Array>], Array<[] | [number]>>,
+  'expand' : ActorMethod<[number, number, Uint8Array | number[]], number>,
+  'find' : ActorMethod<
+    [number, Array<Uint8Array | number[]>],
+    Array<[] | [number]>
+  >,
   'get_authorized' : ActorMethod<[], Array<Principal>>,
   'get_block' : ActorMethod<[number], Block>,
-  'get_certificate' : ActorMethod<[], [] | [Uint8Array]>,
-  'get_history' : ActorMethod<[Uint8Array], Uint32Array>,
-  'get_jurors' : ActorMethod<[number], Array<Uint8Array>>,
+  'get_certificate' : ActorMethod<[], [] | [Uint8Array | number[]]>,
+  'get_history' : ActorMethod<[Uint8Array | number[]], Uint32Array | number[]>,
+  'get_jurors' : ActorMethod<[number], Array<Uint8Array | number[]>>,
   'get_pending' : ActorMethod<[], number>,
-  'get_pool' : ActorMethod<[number, number, number], Array<Uint8Array>>,
+  'get_pool' : ActorMethod<
+    [number, number, number],
+    Array<Uint8Array | number[]>
+  >,
   'get_pool_size' : ActorMethod<[number], number>,
   'get_size' : ActorMethod<[number], number>,
   'length' : ActorMethod<[], number>,
-  'remove' : ActorMethod<[Array<Uint8Array>, Uint8Array], number>,
-  'select' : ActorMethod<[number, number, Uint8Array], number>,
+  'remove' : ActorMethod<
+    [Array<Uint8Array | number[]>, Uint8Array | number[]],
+    number
+  >,
+  'select' : ActorMethod<[number, number, Uint8Array | number[]], number>,
 }
